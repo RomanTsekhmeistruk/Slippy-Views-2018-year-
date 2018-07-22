48c36ab4515352a7dca09d83586981737938587d
Roman Tsekhmeistruk <roman.tsekhmeistruk@betlab.com>
2018-07-22 15:12:16 +0300

    Added BaseActivity class for extending by all activitiy classes

---
+++ app/src/main/java/com/romantsekhmeistruk/mobilunitytest/ui/activities/LaunchActivity.java
@@ -0,0 +1,15 @@
+package com.romantsekhmeistruk.mobilunitytest.ui.activities;
+
+import android.os.Bundle;
+
+import com.romantsekhmeistruk.mobilunitytest.R;
+import com.romantsekhmeistruk.mobilunitytest.ui.activities.base.BaseActivity;
+
+public class LaunchActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launch);
+	}
+}
---
+++ app/src/main/java/com/romantsekhmeistruk/mobilunitytest/ui/activities/base/BaseActivity.java
@@ -0,0 +1,47 @@
+package com.romantsekhmeistruk.mobilunitytest.ui.activities.base;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.romantsekhmeistruk.mobilunitytest.R;
+
+import butterknife.ButterKnife;
+
+public class BaseActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	}
+
+	@Override
+	public void setContentView(int layoutResID) {
+		super.setContentView(layoutResID);
+		ButterKnife.bind(this);
+	}
+
+	public void startFragment(Fragment fragment, boolean addToBackStack) {
+		startFragment(fragment, addToBackStack, null);
+	}
+
+	public void startFragment(Fragment fragment, boolean addToBackStack, int animation[]) {
+		String tag = fragment.getClass().getSimpleName();
+		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+		if (animation != null) {
+			fragmentTransaction.setCustomAnimations(
+					animation[0], animation[1], animation[2], animation[3]);
+		}
+		fragmentTransaction
+				.replace(R.id.container, fragment, tag)
+				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+		if (addToBackStack) {
+			fragmentTransaction.addToBackStack(tag);
+		}
+		fragmentTransaction.commit();
+	}
+}
