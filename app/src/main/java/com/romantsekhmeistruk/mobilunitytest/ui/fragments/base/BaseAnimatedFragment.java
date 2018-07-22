8c8f1d4a7a81d1c25f4734eda0775793443ff400
Roman Tsekhmeistruk <roman.tsekhmeistruk@betlab.com>
2018-07-22 15:53:56 +0300

    Added back button to recycler view and view pager fragments

--- app/src/main/java/com/romantsekhmeistruk/mobilunitytest/ui/fragments/base/BaseAnimatedFragment.java
+++ app/src/main/java/com/romantsekhmeistruk/mobilunitytest/ui/fragments/base/BaseAnimatedFragment.java
@@ -1,4 +1,13 @@
 package com.romantsekhmeistruk.mobilunitytest.ui.fragments.base;
 
+import com.romantsekhmeistruk.mobilunitytest.R;
+
+import butterknife.OnClick;
+
 public class BaseAnimatedFragment extends BaseFragment {
+
+	@OnClick(R.id.back)
+	public void onBackPressed() {
+		getActivity().onBackPressed();
+	}
 }
