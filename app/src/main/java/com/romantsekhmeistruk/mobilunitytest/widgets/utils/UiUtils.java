c7f20461259f459c5283edaa0dd38de947e9c5f5
Roman Tsekhmeistruk <roman.tsekhmeistruk@betlab.com>
2018-07-22 18:12:49 +0300

    Added UiUtils class

---
+++ app/src/main/java/com/romantsekhmeistruk/mobilunitytest/widgets/utils/UiUtils.java
@@ -0,0 +1,17 @@
+package com.romantsekhmeistruk.mobilunitytest.widgets.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class UiUtils {
+
+	public static float dpToPix(Context context, float value) {
+		return dpToPix(context.getResources(), value);
+	}
+
+	public static float dpToPix(Resources resources, float value) {
+		return TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, value, resources.getDisplayMetrics());
+	}
+}
