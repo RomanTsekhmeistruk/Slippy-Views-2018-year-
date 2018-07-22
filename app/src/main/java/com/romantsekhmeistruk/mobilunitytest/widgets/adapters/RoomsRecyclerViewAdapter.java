b5444baf42dbdcc72e96477abcac45812d708afc
Roman Tsekhmeistruk <roman.tsekhmeistruk@betlab.com>
2018-07-22 16:57:59 +0300

    Added rooms recycler view adapter

---
+++ app/src/main/java/com/romantsekhmeistruk/mobilunitytest/widgets/adapters/RoomsRecyclerViewAdapter.java
@@ -0,0 +1,38 @@
+package com.romantsekhmeistruk.mobilunitytest.widgets.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.romantsekhmeistruk.mobilunitytest.R;
+import com.romantsekhmeistruk.mobilunitytest.widgets.views.FontTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RoomsRecyclerViewAdapter extends SimpleAdapter<String, RecyclerView.ViewHolder> {
+
+	@NonNull
+	@Override
+	public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_room, parent, false));
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+		((ViewHolder) holder).roomName.setText(getItem(position));
+	}
+
+	class ViewHolder extends RecyclerView.ViewHolder {
+
+		@BindView(R.id.room_name)
+		FontTextView roomName;
+
+		ViewHolder(View itemView) {
+			super(itemView);
+			ButterKnife.bind(this, itemView);
+		}
+	}
+}
